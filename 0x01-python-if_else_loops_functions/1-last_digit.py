44ee710be965294ecf56072a9980080de8974375
Paulelo <pauladeyemi591@gmail.com>
2022-12-02 21:41:30 -0800

    Udating remote repo

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(f'The number is {number}')
+if number >= 10:
+	number = number % 10
+	if number > 5:
+		print(f'Last digit of {number + 10} is {number} and is greater than 5')
+	elif number == 0:
+		print(f'Last digit of {number + 10} is {number} and is 0')
+	elif number < 6:
+		print(f'Last digit of {number + 10} is {number} and is greater than 0')
+	print(f'The number is {number}')
