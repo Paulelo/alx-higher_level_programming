93e54a1f8066f78ef8811c0e5306b53ccf658f0c
Paulelo <pauladeyemi591@gmail.com>
2022-12-05 02:28:35 -0800

    Hot fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 101):
         fizz = 'Fizz'
         buzz = 'Buzz'
-        fizzbuzz= 'FizzBuzz'
+        fizzbuzzo = 'FizzBuzz'
 
         if ((i % 3) == 0) and ((i % 5) == 0):
             print('{}'.format(fizzbuzz), end=" ")
