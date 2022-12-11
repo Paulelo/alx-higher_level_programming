5869c03c58be76115539507486b85dae4f8f57ae
Paulelo <pauladeyemi591@gmail.com>
2022-12-11 07:46:36 -0800

    Hot fixes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-name = dir(hidden_4)
-for na in filter(lambda x: not x.startswith("_"), name):
-    print(end = ""'{}\n'.format(na))
+if __name__ == "__main__":
+    name = dir(hidden_4)
+    for na in filter(lambda x: not x.startswith("_"), name):
+        print(end=""'{}\n'.format(na))
