a4f525f4cf0495c3df649b0c7bfda90195a95ca2
Paulelo <pauladeyemi591@gmail.com>
2022-12-12 03:24:34 +0000

    Udating remote repo

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+ 
+if __name__ == "__main__":
+    n = len(sys.argv)
+    if n < 2:
+        print('0 arguments.')
+    if n == 2:
+        print('1 argument:')
+        print('1: {}'.format(sys.argv[1]))
+    if n > 2:
+        print('{} arguments:'.format(n-1))
+        for i in range(1, n):
+            print('{}: {}'.format(i, sys.argv[i]))
