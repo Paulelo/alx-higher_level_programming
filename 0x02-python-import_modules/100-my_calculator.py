bfd7374d9951f8502468cc488877ad62ac01e8d5
Paulelo <pauladeyemi591@gmail.com>
2022-12-13 10:43:36 +0000

    Hot fixes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,27 +5,27 @@ if __name__ == "__main__":
     n = len(argv)
     if n != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit (1)
+        exit(1)
     elif (argv[2] == '+'):
         a = int(argv[1])
         b = int(argv[3])
         c = add(a, b)
-        print('{} {} {} = {}'.format(a, argv[2], b, c)
-    elif argv[2] == '-':
+        print('{} {} {} = {}'.format(a, argv[2], b, c))
+    elif (argv[2] == '-'):
         a = int(argv[1])
         b = int(argv[3])
         c = sub(a, b)
         print('{} {} {} = {}'.format(a, argv[2], b, c))
-    elif argv[2] == '*':
+    elif (argv[2] == "*"):
         a = int(argv[1])
         b = int(argv[3])
         c = mul(a, b)
         print('{} {} {} = {}'.format(a, argv[2], b, c))
-    elif argv[2] == '/':
+    elif (argv[2] == '/'):
         a = int(argv[1])
         b = int(argv[3])
         c = div(a, b)
         print('{} {} {} = {}'.format(a, argv[2], b, c))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
-        exit (1)
+        exit(1)
