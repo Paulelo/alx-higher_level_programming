1c4b1af7f9f2a4d0f2196aaa194530c5986db145
Paulelo <pauladeyemi591@gmail.com>
2022-12-24 13:48:56 -0800

    Correcting 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,12 @@
 #!/usr/bin/bash
-
+def max_integer(my_list=[]):
+    i = len(my_list)
+    c = my_list[0]
+    j = 1
+    if i == 0:
+        return (None)
+    while j < (i - 1):
+        if c < my_list[j]:
+            c = my_list[j]
+        j += 1
+    return (c)
