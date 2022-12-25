25bf60df11d9b61d30085b070c21fb1537bb898a
Paulelo <pauladeyemi591@gmail.com>
2022-12-25 14:49:30 -0800

    Solving 14th problem

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* is_palindrome - checks if a linked list is palindrome
+* @head: is pointer to head node of list to be checked
+* Return: 0 if palidrome and 1 if not
+*/
+int is_palindrome(listint_t **head)
+{
