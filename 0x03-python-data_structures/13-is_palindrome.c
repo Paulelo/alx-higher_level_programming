f5c3678ff467bbb62aa54ec515bdde9d107e2b44
Paulelo <pauladeyemi591@gmail.com>
2022-12-28 11:23:06 +0000

    Solving technical interview task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,3 +9,23 @@
 */
 int is_palindrome(listint_t **head)
 {
+	/*listint_t *ele = *head;*/
+	listint_t *move = *head;
+        int *my_arr = NULL;
+	int i = 0;
+	int j = 0;
+
+	if ((move->next == NULL) || (head == NULL))
+		return (1);
+	while (move->next != NULL)
+		my_arr[i] = move->n;
+		move = move->next;
+		/*ele = ele->n;*/
+		i++;
+	while (i)
+		for (j = 0; j <= i; j++)
+			if(my_arr[j] == my_arr[i])
+				i--;
+		return (1);
+	return (0);
+}
