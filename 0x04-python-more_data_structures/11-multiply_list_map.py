e462a64df003605e1b7ef46f787f4f91346767c5
Paulelo <pauladeyemi591@gmail.com>
2023-01-02 05:52:11 +0000

    Resolving issue with check

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    a_list = list(map((lambda x: x * number), my_list)) 
-    return (a_list)
+    return list(map((lambda x: x * number), my_list))
