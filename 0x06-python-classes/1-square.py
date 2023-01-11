48a7bd3991d17939c677fbb2647a101f9dee79e4
Paulelo <pauladeyemi591@gmail.com>
2023-01-11 11:45:49 +0000

    Resolving fails

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """ A class Square that defines a square by: (based on 0-square.py)
+
+        Private instance attribute: size
+        Instantiation with size (no type/value verification)
+        No modules imported """
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0):
+    """  a class Square that defines a square by: (based on 2-square.py)
+
+    Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0):
+    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+    if size is less than 0, raise a ValueError exception with the message size must be >= 0
+    Public instance method: def area(self): that returns the current square area
+    No module was imported """
+        def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
