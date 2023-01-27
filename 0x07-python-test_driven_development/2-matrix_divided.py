7e1f1eb7d531dcd88c49ac2067ef86d75cdb6971
Paulelo <pauladeyemi591@gmail.com>
2023-01-27 05:35:56 -0800

    Updating

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 """This module works with matrix"""
 def matrix_divided(matrix, div):
-    
-
+    if len(matrix) != 2:
+        raise TypeError("matrix must be amatrix(list of lists) of integers/floats")
+    elif len(matrix[0]) != len(matrix[1]):
+        raise TypeError("Each row of matrix must have the same size")
+    elif type(div) not in [int, type]:
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    else:
+        return_matrix = matrix[:]
+        for i in range(0, 2):
+            for j in range(0, len(matrix[i])):
+                return_matrix[i][j] = round((matrix[i][j] / div), 2)
+        return (return_matrix)
