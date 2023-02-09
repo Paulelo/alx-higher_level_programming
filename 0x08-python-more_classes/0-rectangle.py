1c7fc9b3ea5c75a8306490cd59dd1ab2c15e0364
Paulelo <pauladeyemi591@gmail.com>
2023-02-09 23:07:54 +0000

    Entered the world of rectangle classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module just defines
+   an empty class rectangle
+   and does nothing more"""
+
+
+class Rectangle:
+    """This class defines
+       defines a retangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,6 @@
+Doing more cool stuffs with classes and objects
+## Directory
+- 0x08-python-more_classes
+
+## Files
+-
