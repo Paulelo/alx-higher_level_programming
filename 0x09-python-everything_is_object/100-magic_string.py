4f689c81a8223e65824aece91563ee194756451a
Paulelo <pauladeyemi591@gmail.com>
2023-04-01 21:04:39 +0000

    Failing checks resolved

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    word = ['BestSchool']
-    rep = word * magic_string.count
-    return (', '.join(rep))
+    return (', '.join(['BestSchool'] * magic_string.count))
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""This a simple module
+   that does somtin fantastic"""
+
 class LockedClass:
+    """This class does not
+    accept any instance name
+    that is not found in the
+    slots list"""
     __slots__ = ['first_name']
 
