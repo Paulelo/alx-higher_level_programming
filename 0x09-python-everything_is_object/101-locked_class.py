1c59eee667af33a2b988fcacc2a579db04e0cb1d
Paulelo <pauladeyemi591@gmail.com>
2023-04-02 17:40:10 +0000

    Final correction on py everything object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,10 +3,10 @@
 """This a simple module
    that does somtin fantastic"""
 
+
 class LockedClass:
     """This class does not
     accept any instance name
     that is not found in the
     slots list"""
     __slots__ = ['first_name']
-
