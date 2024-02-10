d7e3b81801b7ce81574a54722e9403c37ca6ac77
Paulelo <pauladeye591@gmail.com>
2024-02-10 23:44:26 +0100

    Python Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+##	list lst = []
+#    while (obl.__dict__):
+#		count = 0
+#	    for i in obj.__dict__:
+#            list 
+    return sorted(dir(obj))
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+	def area(self):
+		raise Exception("area() is not implemented")
+	def integer_validator(self, name, value):
+		if type(value) != int:
+			raise TypeError("{} must be an integer" .format(name))
+		if value <= 0:
+			raise ValueError("{} must be greater than 0" .format(name))
+
+class Rectangle(BaseGeometry):
+	def __init__(self, width, height):
+#if self.integer_validator("width", width) and self.integer_validator("height", height):
+			self.__width = 0
+			self.__height = 0
+			self.integer_validator("width", width)
+			self.integer_validator("height", height)
+			self.__width = width
+			self.__height = height
