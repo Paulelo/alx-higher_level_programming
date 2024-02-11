0bdd22e2f8f65f4ccbe6d130aade4fab46d9f81b
Paulelo <pauladeye591@gmail.com>
2024-02-12 00:35:01 +0100

    Documented 0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,13 @@ Public instance method: def lookup(self): that returns sorted list
 No module was imported"""
 
 def lookup(obj):
+	"""
+    Returns a sorted list of attributes and methods of an object.
+
+    Args:
+        obj (object): The object whose attributes and methods are to be looked up.
+
+    Returns:
+        list: A sorted list of attribute and method names of the object.
+	"""
     return sorted(dir(obj))
