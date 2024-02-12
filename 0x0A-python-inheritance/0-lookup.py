b9358bb55e56db895345adfc946e376206e60ea9
Paulelo <pauladeye591@gmail.com>
2024-02-12 02:26:45 +0100

    Documented 0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,7 @@ Your module's verbose yet thorough docstring.
 Public instance method: def lookup(self): that returns sorted list
 No module was imported"""
 
+
 def lookup(obj):
+	"""Return a list of an object's available attributes."""
     return sorted(dir(obj))
